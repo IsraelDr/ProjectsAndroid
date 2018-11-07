4f13ed5f7a2ba2d30e6349505e08f5262f8b4934
Israel <Israel@github.com>
2018-11-07 14:57:36 +0200

    MainActivity

--- app/src/main/java/com/example/srulispc/project/controller/MainActivity.java
+++ app/src/main/java/com/example/srulispc/project/controller/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.srulispc.project.controller;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.srulispc.project.R;
 
@@ -12,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
         int k=5;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addRide=findViewById(R.id.addRide);
+        addRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.userdetails_dialog);
+                dialog.setTitle("Title...");
+                dialog.show();
+                Button orderride=findViewById(R.id.orderRide);
+                orderride.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
     }
+
 }
 
