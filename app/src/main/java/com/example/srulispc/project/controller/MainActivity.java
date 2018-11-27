c7121c763a1b9759122689de4b61c71a96a69622
Owner <elad.eli30@gmail.com>
2018-11-27 16:20:33 +0200

    autocomplete and little change in firebase class

--- app/src/main/java/com/example/srulispc/project/controller/MainActivity.java
+++ app/src/main/java/com/example/srulispc/project/controller/MainActivity.java
@@ -6,6 +6,9 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,9 +17,9 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -65,6 +68,8 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
                 dialog.setContentView(R.layout.userdetails_dialog);
                 dialog.show();
 
+
+
                 //---------------Auto complete setting-----------------------
                 PlaceAutocompleteFragment placeAutocompleteFragment;
                 placeAutocompleteFragment = (PlaceAutocompleteFragment)getFragmentManager().findFragmentById( R.id.autocomplete );
@@ -79,6 +84,17 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLoc
 
                     @Override
                     public void onError(Status status) {
+                    }
+                });
+
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        FragmentManager fm = getFragmentManager();
+                        Fragment fragment = (fm.findFragmentById(R.id.autocomplete));
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.remove(fragment);
+                        ft.commit();
                     }
                 });
                 //---------------Make Order Button---------------------------
