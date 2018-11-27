369f845cf2d56325cd35871fcbdf39b5c41d6347
Owner <elad.eli30@gmail.com>
2018-11-27 16:24:45 +0200

    autocomplete and little change in firebase class

--- app/src/main/java/com/example/srulispc/project/controller/MainActivity.java
+++ app/src/main/java/com/example/srulispc/project/controller/MainActivity.java
@@ -17,7 +17,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
